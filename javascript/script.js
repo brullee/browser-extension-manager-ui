ecad28f366d167ab7c93a1c2e6698cd9ea063192
Abdalla <abdalla.wohoush@gmail.com>
2025-08-19 15:49:57 +0300

    Complete Implementation of Filtering & Extension Functionality

--- javascript/script.js
+++ javascript/script.js
@@ -1,20 +1,10 @@
 const handleFilter = document.querySelectorAll(".filter__btn");
-
-handleFilter.forEach((filterButton) => {
-  filterButton.addEventListener("click", () => {
-    handleFilter.forEach((b) => b.classList.remove("btn--selected"));
-    filterButton.classList.toggle("btn--selected");
-  });
-});
+const container = document.getElementById("extensions-list");
+const template = document.getElementById("extension-template");
 
 fetch("../data.json")
   .then((response) => response.json())
   .then((data) => {
-    // extensionsData = data;
-    // renderExtensions("all");
-    const container = document.getElementById("extensions-list");
-    const template = document.getElementById("extension-template");
-
     data.forEach((extension) => {
       const extensionElement = template.content.cloneNode(true);
       const removeElement = extensionElement.querySelector(
@@ -46,3 +36,38 @@ fetch("../data.json")
       container.appendChild(extensionElement);
     });
   });
+
+handleFilter.forEach((filterButton) => {
+  filterButton.addEventListener("click", () => {
+    handleFilter.forEach((b) => b.classList.remove("btn--selected"));
+    filterButton.classList.toggle("btn--selected");
+
+    if (filterButton.classList.contains("filter__all")) {
+      filterExtensions("all");
+    } else if (filterButton.classList.contains("filter__active")) {
+      filterExtensions("active");
+    } else if (filterButton.classList.contains("filter__inactive")) {
+      filterExtensions("inactive");
+    }
+  });
+});
+
+function filterExtensions(filterType) {
+  const allExtensions = document.querySelectorAll(".extensions__extension");
+
+  allExtensions.forEach((extensionElement) => {
+    const toggleButton = extensionElement.querySelector(".extension__switch");
+
+    if (filterType === "all") {
+      extensionElement.classList.remove("hide--extension");
+    } else if (filterType === "active") {
+      toggleButton.classList.contains("toggle--on")
+        ? extensionElement.classList.remove("hide--extension")
+        : extensionElement.classList.add("hide--extension");
+    } else if (filterType === "inactive") {
+      !toggleButton.classList.contains("toggle--on")
+        ? extensionElement.classList.remove("hide--extension")
+        : extensionElement.classList.add("hide--extension");
+    }
+  });
+}
